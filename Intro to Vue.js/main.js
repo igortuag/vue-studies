ee0c000060b1d7aa1bf6077b631835c9802e7e50
Igor Tuag <igortuag@gmail.com>
2020-07-23 19:54:25 -0300

    Event Handling

--- Intro to Vue.js/main.js
+++ Intro to Vue.js/main.js
@@ -9,5 +9,35 @@ var app = new Vue({
     isActive: true,
     isStyled: true,
     isDisabled: false,
+    inventory: 8,
+    onSale: true,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green",
+        variantImage: "./assets/vmSocks-green.jpg",
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue",
+        variantImage: "./assets/vmSocks-blue.jpg",
+      },
+    ],
+    sizes: ["Small", "Medium", "Large"],
+    cart: 0,
+  },
+  methods: {
+    addToCart () {
+      this.cart += 1;
+    },
+    removeToCart() {
+      if (this.cart > 0) {
+        this.cart -= 1;
+      }
+    },
+    updateProduct(variantImage) {
+      this.image = variantImage
+    }
   },
 });
